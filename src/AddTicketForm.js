dbc49ab341f9bffaf885e6931405b517bbed4dd9
jayakrishna36 <jayakrishna.kuruva@cjsstechnologies.com>
2024-08-01 12:32:54 +0530

    frontend overview

---
+++ src/AddTicketForm.js
@@ -0,0 +1,186 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+const AddTicketForm = ({ setAddnewTask, setTasks }) => {
+  const formik = useFormik({
+    initialValues: {
+      selectedOption: "",
+      ticketNumber: "",
+      release: "",
+      description: "",
+    },
+    validationSchema: Yup.object().shape({
+      selectedOption: Yup.string().required("Please select your name"),
+      ticketNumber: Yup.string().required("Ticket number is required"),
+      release: Yup.string().required("Release is required"),
+      description: Yup.string().required("Description is required"),
+    }),
+    onSubmit: (values) => {
+      axios
+        .post("http://localhost:3000/api/todoslist", {
+          name: values.selectedOption,
+          ticketNumber: values.ticketNumber,
+          release: values.release,
+          description: values.description,
+        })
+        .then((response) => setTasks((pv) => [...pv, response.data]));
+
+      setAddnewTask(false);
+    },
+  });
+
+  const handleOptionChange = (event) => {
+    formik.setFieldValue("selectedOption", event.target.value);
+  };
+
+  const options = [
+    { name: "ManojKumar Margam" },
+    { name: "Bhargav Gollu" },
+    { name: "Vinay Manthani" },
+    { name: "Kandhala Rakesh Reddy" },
+    { name: "Jayanth Vankeepuram" },
+    { name: "Jayakrishna Kuruva" },
+    { name: "Anusha Banthu" },
+    { name: "Prakash Chiluka" },
+    { name: "Vikas" },
+    { name: "Adithya pampana" },
+    { name: "Meghana Kandala" },
+  ];
+
+  return (
+    <div>
+      <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-75 flex justify-center items-center">
+        <div className="bg-white p-8 rounded-lg shadow-lg md:w-5/12 lg:w-4/12 w-full md:mx-0 mx-4">
+          <div className="flex justify-between">
+            <h1 className="text-center my-3 font-semibold text-xl">
+              Please Fill The Form
+            </h1>
+            <div
+              className="cursor-pointer"
+              onClick={() => setAddnewTask(false)}
+            >
+              +
+            </div>
+          </div>
+          <form onSubmit={formik.handleSubmit}>
+            <label
+              className="block text-xs font-muli mb-1"
+              htmlFor="selectedOption"
+            >
+              Select Your Name
+            </label>
+            <select
+              id="selectedOption"
+              name="selectedOption"
+              className={`bg-gray-100 w-full p-1.5 text-xs outline-none ${
+                formik.errors.selectedOption && formik.touched.selectedOption
+                  ? "border border-red-500"
+                  : ""
+              }`}
+              onChange={handleOptionChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.selectedOption}
+            >
+              <option value="">Select a name</option>
+              {options.map((user) => (
+                <option key={user.name} value={user.name}>
+                  {user.name}
+                </option>
+              ))}
+            </select>
+            {formik.errors.selectedOption && formik.touched.selectedOption && (
+              <div className="text-red-500 text-xs">
+                {formik.errors.selectedOption}
+              </div>
+            )}
+
+            <div className="mt-5">
+              <label htmlFor="ticketNumber" className="block text-xs">
+                Ticket Number
+              </label>
+              <input
+                className={`w-full outline-none bg-gray-100 p-2 ${
+                  formik.errors.ticketNumber && formik.touched.ticketNumber
+                    ? "border border-red-500"
+                    : ""
+                }`}
+                type="text"
+                id="ticketNumber"
+                name="ticketNumber"
+                placeholder="Enter ticket number"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.ticketNumber}
+              />
+              {formik.errors.ticketNumber && formik.touched.ticketNumber && (
+                <div className="text-red-500 text-xs">
+                  {formik.errors.ticketNumber}
+                </div>
+              )}
+            </div>
+
+            <div className="mt-5">
+              <label htmlFor="release" className="block text-xs">
+                Release
+              </label>
+              <input
+                className={`w-full outline-none bg-gray-100 p-2 ${
+                  formik.errors.release && formik.touched.release
+                    ? "border border-red-500"
+                    : ""
+                }`}
+                type="text"
+                id="release"
+                name="release"
+                placeholder="Enter release"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.release}
+              />
+              {formik.errors.release && formik.touched.release && (
+                <div className="text-red-500 text-xs">
+                  {formik.errors.release}
+                </div>
+              )}
+            </div>
+
+            <div className="mt-5">
+              <label htmlFor="description" className="block text-xs">
+                Description
+              </label>
+              <textarea
+                id="description"
+                rows="5"
+                cols="5"
+                className={`w-full outline-none bg-gray-100 ${
+                  formik.errors.description && formik.touched.description
+                    ? "border border-red-500"
+                    : ""
+                }`}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.description}
+                name="description"
+              />
+              {formik.errors.description && formik.touched.description && (
+                <div className="text-red-500 text-xs">
+                  {formik.errors.description}
+                </div>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              className="bg-blue-400 text-sm w-full py-2 mt-5 rounded-full"
+            >
+              ADD
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddTicketForm;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import AddTicketForm from './AddTicketForm';
+import axios from 'axios';
+import { useFormik } from 'formik';
+import { MdDelete } from "react-icons/md";
+const App = () => {
+  const formik = useFormik({
+    initialValues: {
+      selectedOption: '',
+    },
+    onSubmit: (values) => {
+      console.log('values', values);
+    },
+  });
 
-function App() {
+ 
+
+  const [addnewTask, setAddnewTask] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [filteredTasks, setFilteredTasks] = useState([]);
+
+  const options = [
+    { name: 'ManojKumar Margam' },
+    { name: 'Bhargav Gollu' },
+    { name: 'Vinay Manthani' },
+    { name: 'Kandhala Rakesh Reddy' },
+    { name: 'Jayanth Vankeepuram' },
+    { name: 'Jayakrishna Kuruva' },
+    { name: 'Anusha Banthu' },
+    { name: 'Prakash Chiluka' },
+    { name: 'Vikas' },
+    { name: 'Adithya Pampana' },
+    { name: 'Meghana Kandala' },
+  ];
+
+
+  useEffect(
+    () => {
+   axios.get('http://localhost:3000/api/todoslist')
+      .then(response => {
+        console.log(response, 'response');
+        setTasks(response.data);
+        setFilteredTasks(response.data); 
+      })}, []);
+
+  useEffect(() => {
+    if (formik.values.selectedOption) {
+      setFilteredTasks(tasks.filter(task => task.name === formik.values.selectedOption));
+    } else {
+      setFilteredTasks(tasks);
+    }
+  }, [formik.values.selectedOption, tasks]);
+
+  const handleOptionChange = (event) => {
+    formik.setFieldValue('selectedOption', event.target.value);
+  };
+  const handleRelease=(e)=>{
+    console.log()
+    const filterdItems = tasks.filter(eachTask => eachTask.release.includes(e.target.value))
+    console.log(filterdItems)
+    setFilteredTasks(filterdItems)
+
+  }
+ 
+const handleDelete=(id)=> {
+axios.delete(`http://localhost:3000/api/todoslist/${id}`).
+then(response => {
+console.log(response.data)
+setFilteredTasks(response.data)
+ 
+})
+
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full">
+        <form onSubmit={formik.handleSubmit}>
+          <div className='flex items-center justify-between my-4'>
+          <div>
+            <label className="block text-xs font-muli mb-1" htmlFor="selectedOption">
+              Select Your Name
+            </label>
+            <select
+              id="selectedOption"
+              name="selectedOption"
+              className={`bg-gray-100 w-full p-1.5 text-xs outline-none ${
+                formik.errors.selectedOption && formik.touched.selectedOption ? 'border border-red-500' : ''
+              }`}
+              onChange={handleOptionChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.selectedOption}
+            >
+              <option value="">Select a name</option>
+              {options.map((user) => (
+                <option key={user.name} value={user.name}>
+                  {user.name}
+                </option>
+              ))}
+            </select>
+            {formik.errors.selectedOption && formik.touched.selectedOption && (
+              <div className="text-red-500 text-xs">{formik.errors.selectedOption}</div>
+            )}
+          </div>
+          <div>
+            <label htmlFor='release' className='text-xs block'>release</label>
+            <input type="text" id="release" name="release" placeholder="search to release" onChange={handleRelease}/>
+          </div>
+          </div>
+        </form>
+        <button
+          onClick={() => setAddnewTask(true)}
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
         >
-          Learn React
-        </a>
-      </header>
+          Add Task
+        </button>
+
+        <ul className="mt-6 space-y-4">
+          {filteredTasks.map((eachTask) => (
+            <li key={eachTask.ticketNumber} className="bg-white p-4 rounded-lg shadow-sm border border-gray-200">
+              
+              <div className="flex justify-between items-start mb-2">
+                <p className="text-gray-700 font-semibold text-lg">{eachTask.name}</p>
+                <p className="text-gray-500 text-sm">{eachTask.status}</p>
+              </div>
+
+              
+              <div className="flex justify-between text-gray-600 text-sm mb-2">
+                <p>Ticket Number: {eachTask.ticketNumber}</p>
+                <p>Release: {eachTask.release}</p>
+              </div>
+
+              
+              <div className="flex justify-between items-center text-gray-600 text-sm mb-2">
+                <p className="text-gray-700">{eachTask.description}</p>
+                <p className='text-2xl cursor-pointer' onClick={()=>handleDelete(eachTask._id)}><MdDelete/></p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {addnewTask &&
+        <AddTicketForm
+          setAddnewTask={setAddnewTask}
+          tasks={tasks}
+          setTasks={setTasks}
+        />}
     </div>
   );
-}
+};
 
 export default App;
