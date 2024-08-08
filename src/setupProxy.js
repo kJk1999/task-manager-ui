26e2d94f0ed68e9145d26938c0aaf5959a436365
KJK1999 <kuruvajayakrishna026@gmail.com>
2024-08-08 18:12:31 +0530

    url changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3000', // Your backend server
+      target: 'https://task-manager-xgmq.onrender.com/', // Your backend server
       changeOrigin: true,
     })
   );
